b56fda7fe5cc0389e32f0c1bab7adf5c1f8d71e7
Usman Saqlain <usaqlain01@gmail.com>
2026-02-15 18:11:37 -0600

    👻 initial release

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 960,
+    height: 600,
+    transparent: true,
+    frame: false,
+    hasShadow: false,
+    alwaysOnTop: true,
+    resizable: true,
+    title: 'Specter',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile('index.html');
+
+  // Allow clicking through fully transparent regions
+  ipcMain.on('set-ignore-mouse', (_e, ignore) => {
+    win.setIgnoreMouseEvents(ignore, { forward: true });
+  });
+
+  ipcMain.on('set-always-on-top', (_e, on) => {
+    win.setAlwaysOnTop(on);
+  });
+
+  win.on('closed', () => { win = null; });
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
